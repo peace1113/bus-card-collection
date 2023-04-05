dcc694babed7caeedf5bc16f9af461d5469961de
Peace Wang 王詠平 <pwang@continental-consultingltd.com>
2023-04-05 23:49:13 +0800

    modify login process

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,10 @@
 import * as env from '$env/static/private';
 import type User from '../models/User';
+import type LineToken from '../models/LineToken';
+import LineIdToken from '../models/LineIdToken';
+import * as jwt from 'jsonwebtoken';
+
+const token_uri = 'https://api.line.me/oauth2/v2.1/token';
 
 const lineLoginLink : string = (`https://access.line.me/oauth2/v2.1/authorize?response_type=code
 &client_id=${env.LINE_client_id}
@@ -7,26 +12,80 @@ const lineLoginLink : string = (`https://access.line.me/oauth2/v2.1/authorize?re
 &state=login
 &scope=openid%20profile`);
 
-export const load = async ({url, cookies, fetch}) => {    
+const tokenFormBody : URLSearchParams = new URLSearchParams();
+tokenFormBody.append('grant_type', 'authorization_code');
+tokenFormBody.append('code', '');
+tokenFormBody.append('redirect_uri', env.LINE_redirect_uri);
+tokenFormBody.append('client_id', env.LINE_client_id);
+tokenFormBody.append('client_secret', env.LINE_client_secret);
+
+export const load = async ({url, cookies, fetch}) => {   
+
     const code = url.searchParams.get('code');
     const loginCookie = cookies.get('lineId');
-    //LineAuth
-    if(code != null){        
-        const response  = await fetch('/api/Login?code=' + code);
-        const loginResult = (await response.json() as User) ?? null;
-        if(loginResult){
-            console.log(loginResult);
-            cookies.set('lineId', loginResult.sub);
-            cookies.set('lineName', loginResult.lineName);
-            cookies.set('linePicture', loginResult.picture);
-            return {status: 200, login:{name: loginResult.lineName, picture: loginResult.picture}}
+
+    //decode line token
+    if(code != null){
+        tokenFormBody.set('code', code);     
+        const response = await fetch(token_uri, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: tokenFormBody
+        }).then(async (res) => {
+            if(res.ok){
+                let token = (await res.json()) as LineToken;
+                let userInfo = jwt.decode(token.id_token) as LineIdToken;
+                return LineIdToken;
+            }
+            else{
+                return null;
+            }
+        }).catch((reason)=>{
+            console.log(reason);
+            return null;
+        });
+
+        if(response != null){
+  
         }
-        return {status: 404, body: "非巴士團成員"};
+        else{
+            return {status: 404, body: "非巴士團成員"};
+        }
     }
-    else if(loginCookie != null){
-        return {status: 200, login:{name: cookies.get('lineName'), picture: cookies.get('linePicture')}};
+
+    if(loginCookie != null){
+
     }
-    else {
-        return {status: 203, body: "請登入", loginLink: lineLoginLink};
-    }    
+    // if(res.ok){
+    //     const loginResult = (await res.json() as User) ?? null;
+    //     if(loginResult){
+    //         console.log(loginResult);
+    //         JSON.stringify(loginResult);
+    //         cookies.set('lineId', loginResult.sub, {httpOnly: true, path: "/", sameSite: "strict"});
+    //         cookies.set('lineName', loginResult.lineName);
+    //         cookies.set('linePicture', loginResult.picture);
+    //         return {status: 200, login:{name: logisnResult.lineName, picture: loginResult.picture}}
+    //     }
+    //     return {status: 404, body: "非巴士團成員"};
+    // }
+    // else{
+    //     return {status: 404, body: "非巴士團成員"};
+    // }
+}
+
+async function loginByLineIdToken(request : LineIdToken){
+    const response = 
+        await fetch('/api/Login', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(request)
+    }).then(async (res) => {
+        return 
+    }).catch((reason)=>{
+
+    });
 }
